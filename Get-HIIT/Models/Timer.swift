d4aca79a87927934efdbee5ae7cd2fd6ad726f42
Leah <musicalshorty65@gmail.com>
2019-08-19 10:24:38 -0600

    timer updated

--- Get-HIIT/Models/Timer.swift
+++ Get-HIIT/Models/Timer.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+class Timer: NSObject {
+    
+    var timer: Timer?
+    var timeRemaining: TimeInterval?
+    var isOn: Bool? {
+        return timeRemaining != nil
+    }
+    
+    var timeAsString: String {
+        let timeRemaining = Int(self.timeRemaining ?? 20 * 60)
+        let minutesRemaining = timeRemaining / 60
+        let secondsRemaining = timeRemaining - (minutesRemaining * 60)
+        return String(format: "%02d : %02d", arguments: [minutesRemaining, secondsRemaining])
+    }
+    
+}
