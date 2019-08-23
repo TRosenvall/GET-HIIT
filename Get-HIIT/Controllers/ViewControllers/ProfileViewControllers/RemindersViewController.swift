484eff11446dcced6ed95731e3cc31b3f82c58b2
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2019-08-22 19:57:55 -0600

    Added graphing pod, pulled some stuff out on a storyboard.

--- Get-HIIT/Controllers/ViewControllers/ProfileViewControllers/RemindersViewController.swift
+++ Get-HIIT/Controllers/ViewControllers/ProfileViewControllers/RemindersViewController.swift
@@ -7,10 +7,103 @@
 //
 
 import UIKit
+import UserNotifications
 
 class RemindersViewController: UIViewController {
+    
+    @IBOutlet weak var remindersSwitch: UISwitch!
+    @IBOutlet weak var reminderTimePicker: UIDatePicker!
+    @IBOutlet weak var remindMeLabel: UILabel!
+    @IBOutlet weak var enableInSettingsButton: UILabel!
+    
+    let notificationCenter = UNUserNotificationCenter.current()
+    let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+    let profile = ProfileController.sharedInstance.profile
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        notificationCenter.delegate = self as? UNUserNotificationCenterDelegate
+        setupViews()
+    }
+    
+    @IBAction func backButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func remindersSwitchTapped(_ sender: Any) {
+        requestNotificationAuthorization()
+        setupViews()
+    }
+    
+    func setupViews() {
+        notificationCenter.getNotificationSettings { (settings) in
+            let auth = settings.authorizationStatus
+            DispatchQueue.main.async {
+                if auth == .denied {
+                    self.remindersSwitch.isEnabled = false
+                } else {
+                    self.remindersSwitch.isEnabled = true
+                }
+            }
+        }
+        remindersSwitch.isOn = profile.notificationsEnabled
+        remindMeLabel.isHidden = !profile.notificationsEnabled
+        reminderTimePicker.isHidden = !profile.notificationsEnabled
+    }
+    
+    
+    func requestNotificationAuthorization() {
+        notificationCenter.requestAuthorization(options: options) {
+            (didAllow, error) in
+            if let error = error {
+                print("\(error.localizedDescription)")
+                print("Error requesting notification authorization")
+            }
+            
+            DispatchQueue.main.async {
+                if !didAllow {
+                    print("User has declined notifications")
+                    self.remindersSwitch.isOn = false
+                    self.remindersSwitch.isEnabled = false
+                }
+            }
+        }
+    }
+    
+    func getNotificationAuthorizationStatus() {
+        notificationCenter.getNotificationSettings { (settings) in
+            if settings.authorizationStatus != .authorized {
+                // Notifications not allowed
+            }
+        }
+    }
+    
+    func scheduleNotification(notificationType: String) {
+        
+        let content = UNMutableNotificationContent()
+        
+        content.title = notificationType
+        content.body = "This is example how to create " + notificationType
+        content.sound = UNNotificationSound.default
+        content.badge = 1
+        
+        let date = Date(timeIntervalSinceNow: 3600)
+        let triggerDate = Calendar.current.dateComponents([.hour,.minute,.second], from: date)
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDate, repeats: false)
+        
+        let identifier = "Local Notification"
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        
+        notificationCenter.add(request) { (error) in
+            if let error = error {
+                print("Error \(error.localizedDescription)")
+            }
+        }
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound])
     }
 }
