7d1de94c25890decf75110498282377053ad5c10
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2019-08-28 15:06:11 -0600

    Workout Main Storyboard

--- Get-HIIT/Controllers/ViewControllers/Skeleton/WorkoutTimerViewController.swift
+++ Get-HIIT/Controllers/ViewControllers/Skeleton/WorkoutTimerViewController.swift
@@ -130,7 +130,7 @@ class WorkoutTimerViewController: UIViewController, CountdownTimerDelegate {
         }
         
         let gradient: CAGradientLayer = CAGradientLayer()
-        gradient.colors = [color1.cgColor, color2.cgColor]
+        gradient.colors = [color2.cgColor, color1.cgColor]
         gradient.locations = [0.0 , 1.0]
         gradient.startPoint = CGPoint(x: 0.0, y: 0.0)
         gradient.endPoint = CGPoint(x: 0.0, y: 1.0)
