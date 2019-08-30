b2a424865884a7b664537d305fd9d377853769d2
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2019-08-29 20:33:22 -0600

    views completed

--- Get-HIIT/Controllers/ViewControllers/My Plan View Controllers/UpcomingWorkoutsViewController.swift
+++ Get-HIIT/Controllers/ViewControllers/My Plan View Controllers/UpcomingWorkoutsViewController.swift
@@ -10,21 +10,73 @@ import UIKit
 
 class UpcomingWorkoutsViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var titleView: UIView!
+    @IBOutlet weak var planTableView: UITableView!
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return .lightContent
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.isNavigationBarHidden = true
+        SetGradient.setGradient(view: titleView, chooseTwo: true, primaryBlue: false, accentOrange: true, accentBlue: false)
+        titleView.layer.shadowOpacity = 0.3
+        titleView.layer.shadowOffset = CGSize(width: 0, height: 3)
+        // Do any additional setup after loading the view.
+        planTableView.delegate = self
+        planTableView.dataSource = self
     }
-    */
+    
+    @IBAction func pastButtonTapped(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "MyPlan", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "PastWorkoutsStoryboard")
+        self.navigationController?.pushViewController(viewController, animated: false)
+    }
+    
+    @IBAction func createButtonWorkoutTapped(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "MyPlan", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "CreateWorkoutStoryboard")
+        self.present(viewController, animated: true, completion: nil)
+    }
+}
 
+extension UpcomingWorkoutsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return WorkoutsController.sharedInstance.totalWorkouts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "upcomingWorkoutCell", for: indexPath) as! UpcomingWorkoutsTableViewCell
+        
+        let workout = WorkoutsController.sharedInstance.totalWorkouts[indexPath.row]
+        
+        cell.workoutNameLabel.text = workout.name
+        cell.workout1Label.text = workout.workouts[0].name
+        cell.workout2Label.text = workout.workouts[1].name
+        cell.workout3Label.text = workout.workouts[2].name
+        cell.workout4Label.text = workout.workouts[3].name
+        
+        var totalTime: Int = 0
+        for exercise in workout.workouts {
+            totalTime += exercise.duration + exercise.rest
+        }
+        totalTime *= workout.multiplier
+        let minutesLabel = "\(totalTime/60)"
+        let secondsLabel = "\(totalTime%60)"
+        cell.totalTimeLabel.text = "\(minutesLabel):\(secondsLabel)"
+        
+        cell.layer.cornerRadius = cell.frame.height/5
+        cell.layer.borderColor = UIColor.getHIITGray.cgColor
+        cell.layer.borderWidth = 2
+        
+        cell.bounds.insetBy(dx: 0, dy: 3)
+        
+        return cell
+    }
+    
+    
+    
+    
 }
--- Get-HIIT/Views/My Plan Cells/SelectWorkoutTableViewCell.swift
+++ Get-HIIT/Views/My Plan Cells/SelectWorkoutTableViewCell.swift
@@ -10,15 +10,7 @@ import UIKit
 
 class SelectWorkoutTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    @IBOutlet weak var workoutNameLabel: UILabel!
+    @IBOutlet weak var infoButton: UIButton!
+    @IBOutlet weak var borderView: UIView!
 }
